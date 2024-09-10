239a49c201070a2a39f4b1cf410f36d310084628
Pyro Nicampt <81565698+PyroNicampt@users.noreply.github.com>
2024-09-10 17:45:18 -0500

    Make application single-instance

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.Threading;
 
 namespace Calcuhandy {
     internal sealed class Program {
@@ -7,8 +8,16 @@ namespace Calcuhandy {
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args) {
+            //Mutex name is appended with the MD5 of "PyroNicamptCalcuhandy"
+            using(Mutex lockMutex = new Mutex(false, "Calcuhandy_55afd9bf")) {
+                if(!lockMutex.WaitOne(0, false)) {
+                    return;
+                }
+                BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+            }
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp() =>
